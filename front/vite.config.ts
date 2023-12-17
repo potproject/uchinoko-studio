d073517f20fa47ec20208cd4a55996bdb214e469
potproject <pptppctt@gmail.com>
2023-12-18 00:30:27 +0900

    https only

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [ sveltekit(),mkcert() ],
+	server: { https: true }
 });
